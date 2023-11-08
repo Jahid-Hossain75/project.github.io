87f374e3e1c4430eec2ae0c3c650c0363fd53ac3
Jahid Hossain <107812081+Jahid-Hossain75@users.noreply.github.com>
2023-11-08 20:38:27 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+function menufunction(){
+    let nav = document.getElementById('nav_items').classList.toggle('show');
+    let search_b = document.getElementById('search_btn').style.display="block";
+}
